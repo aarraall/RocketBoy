c560219ab904fe7602cfeea7e76f021dd106d5ec
aral burak dogan <aralburakdogan@gmail.com>
2019-06-26 16:01:08 +0300

    Thrust sound effect added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rB;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rB = GetComponent<Rigidbody>();
-
+        audioSource = GetComponent<AudioSource>();
 
     }
 
@@ -22,18 +23,28 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-            print("Rotating left");
-        }else if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(-Vector3.forward);
-            print("Rotating right");
-        }
         if (Input.GetKey(KeyCode.Space))
         {
-            rB.AddRelativeForce(Vector3.right);
-            print("Sağa döndü");
+            rB.AddRelativeForce(Vector3.up);
+            print("Rotating up");
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }else
+        {
+            audioSource.Stop();
+        }
+        
+        
+        if(Input.GetKey(KeyCode.A)){
+            transform.Rotate(Vector3.forward);
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+            print("Rotating right");
         }
     }
 }
